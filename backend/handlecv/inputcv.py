1db6d3307d5adfe44cca5cf4543efec144dcca4e
İnan Derin Akın <inandakin@gmail.com>
2026-07-10 02:30:25 +0300

    Made the CV text extraction to a function which gives all the lines of the CV as a list.

--- backend/handlecv/inputcv.py
+++ backend/handlecv/inputcv.py
@@ -1,43 +1,44 @@
 import boto3
 from dotenv import load_dotenv
-import time, sys
+import time
+from collections import Counter
 
 load_dotenv()
 
 client = boto3.client("textract")
 
-print(client.meta.region_name)
+def extract_cv_text(bucket: str, key: str) -> list[str]:
+    response = client.start_document_text_detection(
+        DocumentLocation = {
+            'S3Object': {
+            'Bucket': bucket,
+            'Name': key
+            }
+        }
+    )
 
-response = client.start_document_text_detection(
-DocumentLocation={
-    'S3Object': {
-        'Bucket': 'calibrate-teamthrow',
-        'Name': 'dev/test-cv.pdf'
-    }
-},
-)
+    isSuccess = ""
+    attemptCount= 0
+    print("Waiting for results...")
+    while (isSuccess != "SUCCEEDED"):
+        attemptCount += 1
+        if (attemptCount< 30):
+            result = client.get_document_text_detection(JobId=response["JobId"])
+            isSuccess = result["JobStatus"]
 
-isSuccess = ""
-attemptCount= 0
+            if isSuccess == "SUCCEEDED":
+                print("Success!")
+                break
 
-while (isSuccess != "SUCCEEDED"):
-    attemptCount += 1
-    if (attemptCount< 30):
+            elif isSuccess == "FAILED":
+                raise RuntimeError("Please try again later...") 
 
-        result = client.get_document_text_detection(JobId=response["JobId"])
-        isSuccess = result["JobStatus"]
+            time.sleep(5)
+        else:
+            raise RuntimeError("Max attempt reached. Please try again later...")
+    
+    line_list = [b["Text"] for b in result["Blocks"] if b["BlockType"] == "LINE"]
+    return line_list
 
-        print("Checking success... " + isSuccess)
-
-        if isSuccess == "SUCCEEDED":
-            print("Success!")
-            break
-        elif isSuccess == "FAILED":
-            sys.exit("Please try again later")
-
-        time.sleep(5)
-    else:
-        sys.exit("Max attempt reached. Please try again later.")
-
-
-print(len(result["Blocks"]))
+if __name__ == "__main__":
+    print(extract_cv_text("calibrate-teamthrow", "dev/test-cv.pdf"))
