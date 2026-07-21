69c95215d7a835c357aa644f3fe43b775a1aa80b
İnan Derin Akın <inandakin@gmail.com>
2026-07-21 11:46:11 +0300

    Wired up the embedding system

--- backend/handlecv/gaps.py
+++ backend/handlecv/gaps.py
@@ -1,14 +1,14 @@
 from models import NormalizedSkill, GapResult, Gap
 
 def compute_gaps(cv_skills: list[NormalizedSkill], target_roles: list[str], demand_profile: dict[str, list[NormalizedSkill]]) -> GapResult:
-    gaps_list = []
+    gaps_list = {}
     checked_roles = []
     for role in target_roles:
         if role not in demand_profile:
             continue
 
         checked_roles.append(role)
-        
+        gaps_list[role] = []
         profile = demand_profile[role]
         
         for demanded in profile:
@@ -18,7 +18,7 @@ def compute_gaps(cv_skills: list[NormalizedSkill], target_roles: list[str], dema
                     if cv_skill.esco_category == demanded.esco_category:
                         gap.closest_cv_skill = cv_skill.skill
                         break
-                gaps_list.append(gap)
+                gaps_list[role].append(gap)
     return GapResult(target_roles=checked_roles, gaps = gaps_list)
 
 if __name__ == "__main__":
@@ -26,8 +26,14 @@ if __name__ == "__main__":
     java_normalized = NormalizedSkill(skill = "Java", esco_category="programming languages")
     c_normalized = NormalizedSkill(skill = "C", esco_category="programming languages")
     sql_normalized = NormalizedSkill(skill = "SQL", esco_category="database management")
+    
     target_roles = ["Data Scientist", "Software Engineer"]
     cv = [python_normalized, java_normalized]
-    candidates = [java_normalized, c_normalized, sql_normalized]
-    profile = {"Data Scientist": candidates}
-    print(compute_gaps(cv_skills = cv, target_roles = target_roles, demand_profile = profile))
+
+    profile = {
+        "Data Scientist": [java_normalized, c_normalized, sql_normalized],
+        "Software Engineer": [java_normalized],
+    }
+
+    result = compute_gaps(cv_skills = cv, target_roles = target_roles, demand_profile = profile)
+    print(result.model_dump_json(indent=2))
